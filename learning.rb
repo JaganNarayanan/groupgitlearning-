29748f2662b91c7c116344107b5dc51e93c80dce
jasonlohch <jasonlohch@gmail.com>
2017-07-24 11:29:41 +0800

    git commit branch

---
+++ learning.rb
@@ -0,0 +1,9 @@
+git init
+git status
+git add .
+git commit
+-enter commit message
+-ctrl X
+-Y
+-copy paste "git remote add origin https://github.com/jasonlohch/repository_name.git" from github
+git push origin jason_update
